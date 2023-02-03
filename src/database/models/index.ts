1384d3f4fc820e80e6475f7bfe43e93e9a0d46ed
AlexChirkov1 <13417757+AlexChirkov1@users.noreply.github.com>
2023-02-03 10:10:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/database/models/index.ts
@@ -0,0 +1,11 @@
+import { File } from './file.model';
+import { Gerdan } from './gerdan.model';
+import { Pixel } from './pixel.model';
+import { User } from './user.model';
+
+export default [
+    User,
+    Pixel,
+    Gerdan,
+    File,
+];
