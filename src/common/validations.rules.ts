a618f7b61a078020ec755986282cbd60c0dd9d75
DustTail <dusttailtale@gmail.com>
2023-02-04 10:05:02 +0200

    Updated password regex

--- src/common/validations.rules.ts
+++ src/common/validations.rules.ts
@@ -1,7 +1,7 @@
 export const validationRules = {
     usernameMinLength: 3,
     stringMaxLength: 255,
-    passwordRegex: /^[a-zA-Z0-9]{6,30}$/,
+    passwordRegex: /^[a-zA-Z0-9!@#$%^&*()_\-+=]{6,30}$/,
     usernameRegex: /^[a-zA-Z0-9_]{3,255}$/,
     defaultPagination: 20,
 };
